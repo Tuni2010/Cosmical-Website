9b721722ba25462610cb2844045e7678b47afb79
tuni2010 <tunakayaerdal@icloud.com>
2025-12-18 21:30:22 +0100

    fixed about us

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,6 +154,8 @@ function initActiveLinkHighlighting() {
     });
 }
 
+
+
 // ============================================
 // SMOOTH SCROLL FOR ANCHOR LINKS
 // ============================================
